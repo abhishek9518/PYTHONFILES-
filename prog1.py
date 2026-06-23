786dd6a3fdde4aae537e92ec514652c620911f15
abhishek9518 <abhiawasthi9518@gmail.com>
2026-06-23 19:04:16 +0530

    program file 1

---
+++ prog1.py
@@ -0,0 +1,9 @@
+# Python program for finding average of two numbers.
+a=float(input("enter first number:"))
+b=float(input("enter second number:"))
+print("the average of two numbers is",(a+b)/2)
+
+
+#Python program for finding the square of a number.
+x=int(input("enter a number:"))
+print("the square of the number is:",x**2)
