fb2c68571eeb6a2a54e7e90e9172272abc1040c1
abhishek9518 <abhiawasthi9518@gmail.com>
2026-06-23 17:42:58 +0530

    new file

---
+++ var&data.py
@@ -0,0 +1,42 @@
+# Arithmetic operatiors 
+a=10
+b=20
+c=a+b
+print("the sum of a and b is",c)
+
+#assignment operators
+a = 10-4
+print("the value of a is",a)
+b= 10
+print("the value of b is",b)
+c=20
+c+=10
+print("the value of c is",c)
+
+#comparison operators
+a=10
+b=20
+print(a==b)
+print(a<=b)
+
+#logical operators  
+a = True
+b = False
+print(a and b)
+print(not(False)) 
+
+a=30
+print(type(a))
+
+a="30"
+b=int(a)
+print(type(b))
+
+p=int(input("enter number 1:"))
+q=int(input("enter number 2:")) 
+print("the sum of p and q is",p+q)
+
+
+x=int(input("enter a number:"))
+y=3
+print("the remainder is:",x%y)
