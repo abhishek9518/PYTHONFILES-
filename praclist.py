3fc9515ac13e15fe7a34e90ebbd965b94c2f7583
abhishek9518 <abhiawasthi9518@gmail.com>
2026-06-29 19:01:24 +0530

    prac

---
+++ praclist.py
@@ -0,0 +1,48 @@
+#make a user fruit list 
+'''
+marks = []
+f1 = int(input("enter marks:"))
+marks.append(f1)
+f1 = int(input("enter marks:"))
+marks.append(f1)
+f1 = int(input("enter marks:"))
+marks.append(f1)
+f1 = int(input("enter marks:"))
+marks.append(f1)
+f1 = int(input("enter marks:"))
+marks.append(f1)
+f1 = int(input("enter marks:"))
+marks.append(f1)
+print(marks)
+marks.sort()
+print(marks)
+'''
+#change in data types 
+'''
+x = 10
+print(id(x))
+
+x = 10
+print(id(x))
+
+marks = [10, 20, 30]
+
+print(id(marks))
+
+marks.append(40)
+
+print(id(marks))
+'''
+
+#sum of lists 
+'''
+l = [20,30,50,100]
+print(sum(l))
+'''
+
+#count no. of elements 
+
+list2 = [1,2,3,4,5,2,3,3,3,3,5,5,6,6,]
+total=list2.count(3)
+print(total)
+
